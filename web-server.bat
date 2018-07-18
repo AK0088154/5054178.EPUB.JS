6b55b62874850b55f0f060cb18917dfd55f9584d
KUMAR, ADITYA <ak0088154@TECHMAHINDRA.COM>
2018-07-18 19:40:11 -0400

    Initial commit

---
+++ web-server.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+REM @Author:		Aditya Kumar	    			------- MER
+REM @EmailId:		AK0088154@techmahindra.com		------- MER
+REM -------		'''    '''				------- MER
+node web-server.js
+REM -------		'''    '''				------- MER
